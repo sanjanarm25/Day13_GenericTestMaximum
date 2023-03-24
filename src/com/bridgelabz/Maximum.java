18b9d3ba4895bb52bb4e0f6568963bca42248bfa
sanjanamalkar <sanjanamalkar@gmail.com>
2023-03-24 18:47:03 +0530

    Added:UC5

--- src/com/bridgelabz/Maximum.java
+++ src/com/bridgelabz/Maximum.java
@@ -16,19 +16,26 @@ public class Maximum <T extends Comparable<T>>{
 
     }
 
+    public static <T extends Comparable<T>> void printMax(T max) {
+        System.out.println("Maximum value: " + max);
+
+    }
+
     public static void main(String[] args) {
         // test case for integers
         Maximum<Integer> intMax = new Maximum<>(10, 5, 3, 8, 12);
-        System.out.println(intMax.findMax()); // expected output: 12
+        Integer maxInt = intMax.findMax();
+        printMax(maxInt); // expected output: Maximum value: 12
 
         // test case for floats
         Maximum<Float> floatMax = new Maximum<>(10.5f, 7.2f, 3.8f, 9.1f, 6.7f);
-        System.out.println(floatMax.findMax()); // expected output: 10.5
+        Float maxFloat = floatMax.findMax();
+        printMax(maxFloat); // expected output: Maximum value: 10.5
 
         // test case for strings
         Maximum<String> stringMax = new Maximum<>("Apple", "Peach", "Banana", "Mango", "Cherry");
-        System.out.println("Test case: The maximum string is " + stringMax.findMax());
+        String maxString = stringMax.findMax();
+        printMax(maxString); // expected output: Maximum value: Peach
     }
-
 
     }
