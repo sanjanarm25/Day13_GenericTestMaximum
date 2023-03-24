7f4f3f7b7c5104b88f04a9e4fb64266fc8b2bf35
sanjanamalkar <sanjanamalkar@gmail.com>
2023-03-24 18:40:12 +0530

    Added:UC5

--- src/com/bridgelabz/Maximum.java
+++ src/com/bridgelabz/Maximum.java
@@ -1,39 +1,32 @@
 package com.bridgelabz;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class Maximum <T extends Comparable<T>>{
-    private T a;
-    private T b;
-    private T c;
+    private List<T> values;
 
-    public Maximum(T a, T b, T c) {
-        this.a = a;
-        this.b = b;
-        this.c = c;
+    public Maximum(T... values) {
+        this.values = Arrays.asList(values);
     }
 
     public T findMax() {
-        T max = a;
-        if (b.compareTo(max) > 0) {
-            max = b;
-        }
-        if (c.compareTo(max) > 0) {
-            max = c;
-        }
-        return max;
+        Collections.sort(values);
+        return values.get(values.size() - 1);
 
-}
+    }
 
     public static void main(String[] args) {
         // test case for integers
-        Maximum<Integer> intMax = new Maximum<>(10, 5, 3);
-        System.out.println(intMax.findMax()); // expected output: 10
+        Maximum<Integer> intMax = new Maximum<>(10, 5, 3, 8, 12);
+        System.out.println(intMax.findMax()); // expected output: 12
 
         // test case for floats
-        Maximum<Float> floatMax = new Maximum<>(10.5f, 7.2f, 3.8f);
+        Maximum<Float> floatMax = new Maximum<>(10.5f, 7.2f, 3.8f, 9.1f, 6.7f);
         System.out.println(floatMax.findMax()); // expected output: 10.5
 
         // test case for strings
-        Maximum<String> stringMax = new Maximum<>("Apple", "Peach", "Banana");
+        Maximum<String> stringMax = new Maximum<>("Apple", "Peach", "Banana", "Mango", "Cherry");
         System.out.println("Test case: The maximum string is " + stringMax.findMax());
     }
 
